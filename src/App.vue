bec51de28fa75d2e7984bbcc55c4925adb4377ba
adrhmdlz <adrianalzidan35@gmail.com>
2024-07-30 19:43:42 +0700

    update: Initial new components, added new methods, data, etc..

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
-  <div id="app" class="container-fluid my-5 mx-3">
-    <h1>VueShop</h1>
-    <product-list :products="products" :maximum="maximum"></product-list>
+  <div id="app" class="container my-5">
+    <navigation-bar
+      :carts="carts"
+      :cartQty="cartQty"
+      :cartTotal="cartTotal"
+      @toggle="toggleSliderStatus"
+      @delete="deleteItem"
+    ></navigation-bar>
+    <price-slider :sliderStatus="sliderStatus" :maximum.sync="maximum"></price-slider>
+    <product-list :products="products" :maximum="maximum" @add="addItem"></product-list>
   </div>
 </template>
 
 <script>
 import ProductList from './components/ProductList.vue';
+import NavigationBar from './components/NavigationBar.vue';
+import PriceSlider from './components/PriceSlider.vue';
 
 export default {
   name: 'App',
@@ -14,10 +23,43 @@ export default {
     return {
       maximum: 200,
       products: [],
+      carts: [],
+      sliderStatus: false,
     };
+  },
+  methods: {
+    addItem: function (product) {
+      let productIndex;
+      let productExist = this.carts.filter(function (item, index) {
+        if (Number(item.product.id) === Number(product.id)) {
+          productIndex = index;
+          return true;
+        } else {
+          return false;
+        }
+      });
+
+      if (productExist.length) {
+        this.carts[productIndex].qty++;
+      } else {
+        this.carts.push({ product: product, qty: 1 });
+      }
+    },
+    deleteItem: function (key) {
+      if (this.carts[key].qty > 1) {
+        this.carts[key].qty--;
+      } else {
+        this.carts.splice(key, 1);
+      }
+    },
+    toggleSliderStatus: function () {
+      this.sliderStatus = !this.sliderStatus;
+    },
   },
   components: {
     ProductList,
+    NavigationBar,
+    PriceSlider,
   },
   mounted: async function () {
     try {
@@ -28,7 +70,24 @@ export default {
       console.error(error);
     }
   },
+  computed: {
+    sliderState: function () {
+      return this.style.sliderStatus ? 'd-flex' : 'd-none';
+    },
+    cartTotal: function () {
+      let sum = 0;
+      for (let key in this.carts) {
+        sum = sum + this.carts[key].product.price * this.carts[key].qty;
+      }
+      return sum;
+    },
+    cartQty: function () {
+      let qty = 0;
+      for (let key in this.carts) {
+        qty = qty + this.carts[key].qty;
+      }
+      return qty;
+    },
+  },
 };
 </script>
-
-<style></style>
