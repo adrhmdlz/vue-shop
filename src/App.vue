98dbf60930387c5f8a2eeea561b4dad636f3bf97
adrhmdlz <adrianalzidan35@gmail.com>
2024-07-30 14:29:03 +0700

    update: Initial new components, fetch data from API, restructure code.

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,33 @@
 <template>
-  <div id="app" class="container my-5">
-    <span>Test</span>
+  <div id="app" class="container-fluid my-5 mx-3">
+    <h1>VueShop</h1>
+    <product-list :products="products" :maximum="maximum"></product-list>
   </div>
 </template>
 
 <script>
+import ProductList from './components/ProductList.vue';
+
 export default {
   name: 'App',
-  components: {},
+  data: function () {
+    return {
+      maximum: 200,
+      products: [],
+    };
+  },
+  components: {
+    ProductList,
+  },
+  mounted: async function () {
+    try {
+      const response = await fetch('https://hplussport.com/api/products/order/price');
+      const data = await response.json();
+      this.products = data;
+    } catch (error) {
+      console.error(error);
+    }
+  },
 };
 </script>
 
