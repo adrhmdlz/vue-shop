209904e1e39d4c55b7be9d78559313fcd3bfdfeb
adrhmdlz <adrianalzidan35@gmail.com>
2024-07-30 13:59:17 +0700

    add: Added new component (price-component).

---
+++ src/components/PriceComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <span class="fw-bold">{{
+    this.prefix + Number.parseFloat(this.value).toFixed(this.precision)
+  }}</span>
+</template>
+
+<script>
+export default {
+  props: {
+    prefix: {
+      type: String,
+      default: '$',
+    },
+    value: Number,
+    precision: {
+      type: Number,
+      default: 2,
+    },
+  },
+};
+</script>
